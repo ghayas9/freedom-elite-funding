8013a0ce38759c5cee2661d95daf054709aa6157
ghayas9 <ghayasudin999@gmail.com>
2024-07-23 04:01:19 +0500

    fix

--- src/components/WhatPeopleSay.tsx
+++ src/components/WhatPeopleSay.tsx
@@ -18,29 +18,29 @@ export default function WhatPeopleSay() {
         "An Amazing proop that stands out from thw rest. Freedom Elite Funding offers trader friendly DD, No time limit on all their challenge phases which is top notch. And also pochet friendly challenge cost.",
     },
     {
-      designation:"One of the competitive prop firm is here.." ,
-      name:"Rabbi akhmed" ,
+      designation: "One of the competitive prop firm is here..",
+      name: "Rabbi akhmed",
       description:
         "Professional website and friendly user. Amazing challenges and very affordablel Profit Consistency rule is very good for all the traders to avoid oevertrade when you are already profit to that month..",
     },
     {
-      designation:"Affordable price - I bought with 50% discount" ,
-      name:"Henrylarsen" ,
+      designation: "Affordable price - I bought with 50% discount",
+      name: "Henrylarsen",
       description:
         "Everything is smoother with Freedom elite funding so far. Affordable price, No delay occured for account review after passing phase 1&2, KYC verification and getting my funded account. I didn't had my first withdrawal yet (just got my master account) and great firm!!! Fully recommend",
     },
     {
-      designation:"transparent from day one" ,
-      name:"Alexis landers" ,
+      designation: "transparent from day one",
+      name: "Alexis landers",
       description:
         "Company has been transparent from day one! Even when they lost their meta quotes license the CEO responded literally I messaged him and he responded and then provided us with a few other options other then mt4! I got my payout in 2 days!! Love freedom elite",
     },
     {
-      designation:"Quick payout within 24hours" ,
-      name:"Rezik Yusuf" ,
+      designation: "Quick payout within 24hours",
+      name: "Rezik Yusuf",
       description:
         "I got 2 payouts so far, the first took 2-3 days and the 2nd payout took less than one hour which is crazy. I have tried many propfirms and there has always been a catch like slippage or high commissions but with freedom elite i have had zero negative issues so far. This is my honest",
-    }
+    },
   ];
   return (
     <div className=" pb-8 md:pb-4 px-2">
@@ -59,15 +59,31 @@ export default function WhatPeopleSay() {
               description={item?.description}
             />
           ))}
-
         </Marquee>
       </div>
 
-      <Link href="#">
+      {/* <Link href="#">
         <p className="text-center text-base text-white underline underline-offset-4 mt-4 font-bold">
           Show Latest Reviews
         </p>
-      </Link>
+      </Link> */}
+      <div
+        className="trustpilot-widget justify-center items-center flex"
+        data-locale="en-US"
+        data-template-id="56278e9abfbbba0bdcd568bc"
+        data-businessunit-id="66913400f462e383527992c6"
+        data-style-height="52px"
+        data-style-width="100%"
+      >
+        <a
+          href="https://www.trustpilot.com/review/freedomelitefunding.com"
+          target="_blank"
+          rel="noopener"
+          className="text-center text-base text-white underline underline-offset-4 mt-4 font-bold w-full"
+        >
+          Show Latest Reviews
+        </a>
+      </div>
     </div>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,18 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <script
+          type="text/javascript"
+          src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js"
+          async
+        ></script>
+      </Head>
       <ToastContainer />
       <Component {...pageProps} />
     </>
